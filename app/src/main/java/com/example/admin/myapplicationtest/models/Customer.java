deadcce49cfba184cdfece1d2e2895e0f6b1dac6
memariania <@min12345>
2018-08-06 17:49:59 +0430

    Created Model classes

---
+++ app/src/main/java/com/example/admin/myapplicationtest/models/Customer.java
@@ -0,0 +1,1 @@
+package com.example.admin.myapplicationtest.models;import io.objectbox.annotation.Entity;import io.objectbox.annotation.Id;@Entitypublic class Customer {    @Id public long id;}
