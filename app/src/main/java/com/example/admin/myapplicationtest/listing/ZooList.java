e3d8c64d025febd4a7d54f4b9cfa74939228902f
Amin <amin90memar@gmail.com>
2018-08-09 00:07:02 +0430

    remove constLayout

---
+++ app/src/main/java/com/example/admin/myapplicationtest/listing/ZooList.java
@@ -0,0 +1,15 @@
+package com.example.admin.myapplicationtest.listing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.myapplicationtest.R;
+
+public class ZooList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoo_list);
+    }
+}
